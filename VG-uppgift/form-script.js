8d3871aa9695f6f4a1251f45238cc7f9d94ce480
AnnaSjoberg <anna.sjoberg@yh.nackademin.se>
2023-03-28 11:58:06 +0200

    rättat till länkning

--- VG-uppgift/form-script.js
+++ VG-uppgift/form-script.js
@@ -20,7 +20,7 @@ clear.addEventListener("click", e => {
 
 function clearCart(e){
     e.preventDefault();
-    window.location.href = "index.html";
+    window.location.href = "assortment.html";
     sessionStorage.clear();
 }
 
